14383b83bb828af2e1427755a9ec3e38aeff397c
aibatjetenov <jetenov97@mail.ru>
2021-01-31 20:01:54 +0300

    31/01/2020 AJetenov - refactor

--- 2.3.1. Crud_app/src/main/java/web/config/HibernateConfig.java
+++ 2.3.1. Crud_app/src/main/java/web/config/HibernateConfig.java
@@ -8,16 +8,12 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import web.model.User;
-
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -41,27 +37,6 @@ public class HibernateConfig {
         dataSource.setPassword(env.getProperty("db.password"));
         return dataSource;
     }
-
-//    @Bean
-//    public LocalSessionFactoryBean getSessionFactory() {
-//        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-//        factoryBean.setDataSource(getDataSource());
-//
-//        Properties props=new Properties();
-//        props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-//        props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
-//
-//        factoryBean.setHibernateProperties(props);
-//        factoryBean.setAnnotatedClasses(User.class);
-//        return factoryBean;
-//    }
-//
-//    @Bean
-//    public HibernateTransactionManager getTransactionManager() {
-//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-//        transactionManager.setSessionFactory(getSessionFactory().getObject());
-//        return transactionManager;
-//    }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
@@ -89,9 +64,9 @@ public class HibernateConfig {
         return transactionManager;
     }
 
-//    @Bean
-//    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
-//        return new PersistenceExceptionTranslationPostProcessor();
-//    }
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
 
 }
--- 2.3.1. Crud_app/src/main/java/web/dao/UserDaoImp.java
+++ 2.3.1. Crud_app/src/main/java/web/dao/UserDaoImp.java
@@ -1,54 +1,40 @@
 package web.dao;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.model.User;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
-    //@Autowired
-    //private SessionFactory sessionFactory;
 
     @PersistenceContext
     private EntityManager entityManager;
 
     @Override
     public void add(User user) {
-        //sessionFactory.getCurrentSession().save(user);
         entityManager.persist(user);
     }
 
     @Override
     public List<User> UserList() {
-        //TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-        //return query.getResultList();
         return entityManager.createQuery("SELECT e from User e").getResultList();
     }
 
     @Override
     public User getById(Long id) {
-        //return sessionFactory.getCurrentSession().get(User.class, id);
         return entityManager.find(User.class, id);
     }
 
     @Override
     public void update(User user) {
-        //sessionFactory.getCurrentSession().update(user);
         entityManager.merge(user);
     }
 
     @Override
     public void delete(User user) {
-        //sessionFactory.getCurrentSession().delete(user);
         entityManager.remove(entityManager.contains(user) ? user : entityManager.merge(user));
     }
-
 
 }
